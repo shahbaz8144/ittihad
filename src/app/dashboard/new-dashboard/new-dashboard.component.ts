5dc8d2c58443a7fa6b4dc2c076514790543a7aac
rameshdevbox <101255773+rameshdevbox@users.noreply.github.com>
2025-03-04 14:45:14 +0530

    Fixing arabic issues

--- src/app/dashboard/new-dashboard/new-dashboard.component.ts
+++ src/app/dashboard/new-dashboard/new-dashboard.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { DOCUMENT } from '@angular/common';
+import * as momenth from 'moment-hijri';
 @Component({
   selector: 'app-new-dashboard',
   templateUrl: './new-dashboard.component.html',
   styleUrls: ['./new-dashboard.component.css']
 })
 export class NewDashboardComponent implements OnInit {
+  today: string;
   images = [
     { path: 'https://yrglobaldocuments.blob.core.windows.net/documents/Banner/131/588228_Day-1_App-Banner.jpg' },
     { path: 'https://yrglobaldocuments.blob.core.windows.net/documents/Banner/44/160413_Help%20Desk_001%203.png' },
     { path: 'https://yrglobaldocuments.blob.core.windows.net/documents/Banner/147/235199_DMS-Banner.jpg' },
     { path: 'https://yrglobaldocuments.blob.core.windows.net/documents/Banner/145/690704_Magical%20Gingery.jpg' }
   ];
-  constructor() { }
+  currentLang:"ar"|"en"="ar";
+  constructor(private translate:TranslateService,
+      @Inject(DOCUMENT) private document: Document,
+      private renderer: Renderer2,) {
+        // this.today = new Date().toLocaleDateString('en-GB'); // Format: DD-MM-YYYY
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0];
+        this.today = formattedDate;
+        HeaderComponent.languageChanged.subscribe((lang)=>{
+          localStorage.setItem('language',lang);
+          this.translate.use(lang);
+          this.currentLang = lang ? lang : 'en';
+        this.document.dir = this.currentLang === 'ar' ? 'rtl' : 'ltr';
+        if(lang == 'ar'){
+          this.renderer.addClass(document.body, 'kt-body-arabic');
+          this.today = this.getIslamicDate(this.today);
+        }else if (lang == 'en'){
+          const today = new Date();
+          const formattedDate = today.toISOString().split('T')[0];
+          this.today = formattedDate;
+          this.renderer.removeClass(document.body, 'kt-body-arabic');
+        }
+        const editElement = document.getElementById("editrck");
+      if (editElement) {
+        editElement.textContent = lang === 'ar' ? "إضافة" : "Add";
+      }
+           });
+       }
 
   ngOnInit(): void {
+  }
+
+  getIslamicDate(gregorianDate: string): string {
+    return momenth(gregorianDate, 'YYYY-MM-DD').format('iYYYY/iMM/iDD');
   }
 
 }
