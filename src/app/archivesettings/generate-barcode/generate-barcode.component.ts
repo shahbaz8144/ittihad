59bcae4026efcbd0b758abc850f1557728ed1dd6
srujan-devbox <153588807+srujan-devbox@users.noreply.github.com>
2025-04-08 16:54:47 +0530

    generate chnages

--- src/app/archivesettings/generate-barcode/generate-barcode.component.ts
+++ src/app/archivesettings/generate-barcode/generate-barcode.component.ts
@@ -12,7 +12,38 @@ export class GenerateBarcodeComponent implements OnInit {
     this.selectedOption = option;
   }
   ngOnInit(): void {
-    
+    // Add event listener for the edit button click to add 'active' class
+    document.querySelectorAll('.barcode-edit').forEach(function(editButton) {
+      editButton.addEventListener('click', function() {
+        // Find the nearest parent .barcode-seq-item and add 'active' class
+        var barcodeSeqItem = editButton.closest('.barcode-seq-item');
+        if (barcodeSeqItem) {
+          barcodeSeqItem.classList.add('active');
+        }
+      });
+    });
+
+    // Add event listener for the submit button to remove 'active' class
+    document.querySelectorAll('.btn-chng').forEach(function(submitButton) {
+      submitButton.addEventListener('click', function() {
+        // Find the nearest parent .barcode-seq-item and remove 'active' class
+        var barcodeSeqItem = submitButton.closest('.barcode-seq-item');
+        if (barcodeSeqItem) {
+          barcodeSeqItem.classList.remove('active');
+        }
+      });
+    });
+
+    // Add event listener for the cancel button to remove 'active' class
+    document.querySelectorAll('.btn-cncl').forEach(function(cancelButton) {
+      cancelButton.addEventListener('click', function() {
+        // Find the nearest parent .barcode-seq-item and remove 'active' class
+        var barcodeSeqItem = cancelButton.closest('.barcode-seq-item');
+        if (barcodeSeqItem) {
+          barcodeSeqItem.classList.remove('active');
+        }
+      });
+    });
   }
   generate_barcode_open() {
     document.getElementById("generate_barcode").classList.add("kt-quick-panel--on");
@@ -24,4 +55,6 @@ export class GenerateBarcodeComponent implements OnInit {
     document.getElementsByClassName("side_view")[0].classList.remove("position-fixed");
     document.getElementsByClassName("kt-aside-menu-overlay")[0].classList.remove("d-block");
   }
+
+
 }
