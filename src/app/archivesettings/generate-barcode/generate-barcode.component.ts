05524f01a4dc495b49cb4d3ccfcffafc85b75373
srujan-devbox <153588807+srujan-devbox@users.noreply.github.com>
2025-04-08 13:09:07 +0530

    generate bar code

--- src/app/archivesettings/generate-barcode/generate-barcode.component.ts
+++ src/app/archivesettings/generate-barcode/generate-barcode.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generate-barcode.component.css']
 })
 export class GenerateBarcodeComponent implements OnInit {
-
+  selectedOption: string = 'option1';
   constructor() { }
-
-  ngOnInit(): void {
+  togglebarcode(option: string) {
+    this.selectedOption = option;
   }
-
+  ngOnInit(): void {
+    
+  }
+  generate_barcode_open() {
+    document.getElementById("generate_barcode").classList.add("kt-quick-panel--on");
+    document.getElementsByClassName("side_view")[0].classList.add("position-fixed");
+    document.getElementsByClassName("kt-aside-menu-overlay")[0].classList.add("d-block");
+  }
+  generate_barcode_close() {
+    document.getElementById("generate_barcode").classList.remove("kt-quick-panel--on");
+    document.getElementsByClassName("side_view")[0].classList.remove("position-fixed");
+    document.getElementsByClassName("kt-aside-menu-overlay")[0].classList.remove("d-block");
+  }
 }
